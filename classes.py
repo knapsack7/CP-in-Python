e3633cc4183b93d1d36031b8bdfd1c6abbd0e6cf
knapsack7 <manoj@Manojs-MacBook-Air.local>
2024-06-29 12:34:26 +0530

    class variable and instance variable

--- classes.py
+++ classes.py
@@ -1,19 +1,29 @@
 # python object-oriented programming
 
 class Employee:
+
+    raise_amount = 1.04
+    num_of_emps = 0
     
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@company.com' 
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        Employee.num_of_emps += 1 
     
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
 emp_1 = Employee('Manoj','Verma', 50000)
 emp_2 = Employee('Manish','Verma',80000)
 
 
-print(emp_1.fullname())
 
-print(Employee.fullname(emp_2))
+# print(Employee.__dict__)
+# print(emp_1.__dict__)
+
+print(Employee.num_of_emps)
